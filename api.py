83c143631e1c38ccb976a4a668f3c3691f231d7f
Frédéric Meinnel <frederic.meinnel@gmail.com>
2020-03-16 23:37:09 +0100

    Loading players fixed. Added ELO rating and leaderboard page.

--- api.py
+++ api.py
@@ -6,8 +6,18 @@ def addPlayer(player):
     db.sadd('players', player)
 
 def getPlayers():
-    players = [player.decode('UTF-8') for player in db.smembers("players")]
-    players.sort()
+    players = []
+    game_id = db.get('game-id').decode('utf-8')
+    last_game_data = db.hgetall(f'game-{game_id}')
+    for player in db.smembers('players'):
+        name = player.decode('utf-8')
+        try:
+            rating = int(last_game_data[f"elo-{name}".encode()].decode('utf-8'))
+        except:
+            rating = 1200
+        players += [{'name': name, 'rating': rating}]
+
+    players.sort(key=lambda x:x['rating'], reverse=True)
     return players
 
 def getGamesKeys():
@@ -29,12 +39,51 @@ def getGames():
 def insertGame(blueDefense, blueOffense, redDefense, redOffense, blueScore, redScore):
     game_id = db.incr('game-id')
 
-    new_game = 'game-%d'%(game_id)
+    last_game_name = f'game-{game_id - 1}'
+    last_game_data = db.hgetall(last_game_name)
+    try:
+        blueDefenseElo = int(last_game_data[f"elo-{blueDefense}".encode()].decode('utf-8'))
+    except:
+        blueDefenseElo = 1200
+    try:
+        blueOffenseElo = int(last_game_data[f"elo-{blueOffense}".encode()].decode('utf-8'))
+    except:
+        blueOffenseElo = 1200
+    try:
+        redDefenseElo = int(last_game_data[f"elo-{redDefense}".encode()].decode('utf-8'))
+    except:
+        redDefenseElo = 1200
+    try:
+        redOffenseElo = int(last_game_data[f"elo-{redOffense}".encode()].decode('utf-8'))
+    except:
+        redOffenseElo = 1200
+
+    blueElo = (blueDefenseElo + blueOffenseElo) / 2
+    redElo = (redDefenseElo + redOffenseElo) / 2
+
+    expectation_blue = 1 / (1 + 10 ** ((redElo - blueElo) / 400))
+    delta_elo = 32 * ((1 if blueScore == 5 else 0) - expectation_blue)
+    delta_elo = round(delta_elo)
+
+    blueDefenseElo += delta_elo
+    blueOffenseElo += delta_elo
+    redDefenseElo  -= delta_elo
+    redOffenseElo  -= delta_elo
+
+    new_game = f'game-{game_id}'
     db.lpush('games', new_game)
+    try:
+        db.hmset(new_game, last_game_data)
+    except:
+        pass
     db.hmset(new_game, {'blueDefense': blueDefense,
                         'blueOffense': blueOffense,
                         'redDefense' : redDefense,
                         'redOffense' : redOffense,
+                        f'elo-{blueDefense}' : blueDefenseElo,
+                        f'elo-{blueOffense}' : blueOffenseElo,
+                        f'elo-{redDefense}' : redDefenseElo,
+                        f'elo-{redOffense}' : redOffenseElo,
                         'blueScore'  : blueScore,
                         'redScore'   : redScore})
 
--- script.js
+++ script.js
@@ -1,43 +1,56 @@
 
 function homePage(){
     $("#nav-home").parent().addClass("active");
+    $("#nav-leaderboard").parent().removeClass("active");
     $("#nav-score-input").parent().removeClass("active");
     $("#score-input").hide();
+    $("#leaderboard").hide();
     $("#home").show();
+};
+
+function leaderboardPage(){
+    $("#nav-home").parent().removeClass("active");
+    $("#nav-leaderboard").parent().addClass("active");
+    $("#nav-score-input").parent().removeClass("active");
+    $("#score-input").hide();
+    $("#leaderboard").show();
+    $("#home").hide();
+    loadPlayers();
 };
 
 function scoreInputPage(){
     $("#nav-home").parent().removeClass("active");
+    $("#nav-leaderboard").parent().removeClass("active");
     $("#nav-score-input").parent().addClass("active");
     $("#score-input").show();
+    $("#leaderboard").hide();
     $("#home").hide();
     loadPlayers();
 };
 
 function loadPlayers(){
+    $("#leaderboard tbody").empty(); // Remove the leaderboard
+    $(".player-option").remove(); // Remove player options from the selectors
+    $(".player-selector").prop('disabled', true);
+    $(".default-player").prop('selected', true);
     $.ajax({
         url: '/api/getPlayers/',
         type: 'GET',
         dataType: 'json',
         success: function(json){
             for(let index in json){
-                $("#Player1").append($('<option/>', { 
-                    value: json[index],
-                    text : json[index]
+                // Add new player options to all four player selectors
+                let player = json[index];
+                $(".player-selector").append($('<option/>', {
+                    class: 'player-option',
+                    value: player['name'],
+                    text : player['name']
                 }));
-                $("#Player2").append($('<option/>', { 
-                    value: json[index],
-                    text : json[index]
-                }));
-                $("#Player3").append($('<option/>', { 
-                    value: json[index],
-                    text : json[index]
-                }));
-                $("#Player4").append($('<option/>', { 
-                    value: json[index],
-                    text : json[index]
-                }));
+
+                // Add an entry to the leaderboard
+                $("#leaderboard tbody").append('<tr><th scope="row">' + player['name'] + '</th><td>' + player['rating'] + '</td></tr>');
             }
+            $(".player-selector").prop('disabled', false);
         }
     });
 
@@ -46,6 +59,7 @@ function loadPlayers(){
 $(document).ready(function(){
     console.log("ready!");
     $("#nav-home").on("click", homePage);
+    $("#nav-leaderboard").on("click", leaderboardPage);
     $("#nav-score-input").on("click", scoreInputPage);
 });
 
