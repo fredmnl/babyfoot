6452fe12cc304a2b3567d34feddf8c6a1292dab1
Frédéric Meinnel <frederic.meinnel@gmail.com>
2020-03-21 11:25:49 +0100

    Added games display. Added insertGames API endpoint.

--- main.py
+++ main.py
@@ -13,3 +13,8 @@ async def read_root():
 @app.get("/api/getGames/")
 async def read_root():
     return api.getGames()
+
+@app.get("/api/insertGame/")
+async def read_root(blueDefense: str, blueOffense: str, redDefense: str, redOffense: str, blueScore: int, redScore: int):
+    api.insertGame(blueDefense, blueOffense, redDefense, redOffense, blueScore, redScore)
+    return {}
--- script.js
+++ script.js
@@ -2,29 +2,47 @@
 function homePage(){
     $("#nav-home").parent().addClass("active");
     $("#nav-leaderboard").parent().removeClass("active");
+    $("#nav-games").parent().removeClass("active");
     $("#nav-score-input").parent().removeClass("active");
-    $("#score-input").hide();
-    $("#leaderboard").hide();
     $("#home").show();
+    $("#leaderboard").hide();
+    $("#games").hide();
+    $("#score-input").hide();
 };
 
 function leaderboardPage(){
     $("#nav-home").parent().removeClass("active");
     $("#nav-leaderboard").parent().addClass("active");
+    $("#nav-games").parent().removeClass("active");
     $("#nav-score-input").parent().removeClass("active");
     $("#score-input").hide();
     $("#leaderboard").show();
+    $("#games").hide();
     $("#home").hide();
     loadPlayers();
+};
+
+function gamesPage(){
+    $("#nav-home").parent().removeClass("active");
+    $("#nav-leaderboard").parent().removeClass("active");
+    $("#nav-games").parent().addClass("active");
+    $("#nav-score-input").parent().removeClass("active");
+    $("#score-input").hide();
+    $("#leaderboard").hide();
+    $("#games").show();
+    $("#home").hide();
+    loadGames();
 };
 
 function scoreInputPage(){
     $("#nav-home").parent().removeClass("active");
     $("#nav-leaderboard").parent().removeClass("active");
+    $("#nav-games").parent().removeClass("active");
     $("#nav-score-input").parent().addClass("active");
-    $("#score-input").show();
-    $("#leaderboard").hide();
     $("#home").hide();
+    $("#leaderboard").hide();
+    $("#games").hide();
+    $("#score-input").show();
     loadPlayers();
 };
 
@@ -56,10 +74,33 @@ function loadPlayers(){
 
 };
 
+function loadGames(){
+    $("#games tbody").empty(); // Remove the leaderboard
+    $.ajax({
+        url: '/api/getGames/',
+        type: 'GET',
+        dataType: 'json',
+        success: function(json){
+            for(let index in json){
+                // Add new player options to all four player selectors
+                let game = json[index];
+                // Add an entry to the leaderboard
+                $("#games tbody").append('<tr><th scope="row">' +
+                            index + '</th><td>' +
+                            game['blueDefense'] + '</td><td>' + game['blueOffense'] + '</td><td>' +
+                            game['blueScore'] + '</td><td>' + game['redScore'] + '</td><td>' +
+                            game['redOffense'] + '</td><td>' + game['redDefense'] + '</td></tr>');
+            }
+        }
+    });
+
+};
+
 $(document).ready(function(){
     console.log("ready!");
     $("#nav-home").on("click", homePage);
     $("#nav-leaderboard").on("click", leaderboardPage);
+    $("#nav-games").on("click", gamesPage);
     $("#nav-score-input").on("click", scoreInputPage);
 });
 
